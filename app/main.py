d8e41e96df6d254c5a167ca253e076e66fe0cdd1
Erionis <erion.islamay@gmail.com>
2023-11-08 23:33:03 +0100

    updates

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware # 
 
 from pydantic import BaseModel
 
@@ -8,7 +9,7 @@ from .model.model import __version__ as model_version
 
 app = FastAPI()
 origins = [
-    "*"
+    "*"  # allow everybody to send request
 ]
 
 app.add_middleware(
@@ -19,6 +20,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# create a class where the inner parameters are the features
 class HealthStats(BaseModel):
 	Gender:int
 	Age:float
@@ -28,12 +30,14 @@ class HealthStats(BaseModel):
 	Heart_Rate:float
 	Body_Temp:float
 
-@app.get("/")
-def home():
+# our request can be a GET and POST
+# HTTP GET they want to read some data
+@app.get("/")  # create a ENDPOINT to the root of the server
+def home(): # this function will be activated
     return {"health_check": "OK", "model_version": model_version}
-
-@app.post("/predict")
-def predict(payload: HealthStats):
+# HTTP POST, they are sending data to me
+@app.post("/predict")  # ENDPOINT
+def predict(payload: HealthStats): # this function will be activated
     health_stats = [payload.Gender,payload.Age,payload.Height,payload.Weight,payload.Duration,payload.Heart_Rate,payload.Body_Temp]
     return {"burned calories": predict_pipeline(health_stats=health_stats)}
 
